28f63000d268526ebca6d14d332365bf9145b22e
Chua Wei Zhe <chuazhe@gmail.com>
2019-08-27 15:31:23 +0800

    Initial commit

---
+++ SmartMaintenanceApp/SmartMaintenanceApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SmartMaintenanceApp.Models;
+
+namespace SmartMaintenanceApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
